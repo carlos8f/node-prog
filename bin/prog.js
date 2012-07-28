4358bf0db116468dcc42aa0940debf8ddca4c4d0
Carlos Rodriguez <carlos@s8f.org>
2012-07-27 17:53:31 -0700

    Initial code

---
+++ bin/prog.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var argv = require('optimist')
+    .alias('h', 'help')
+    .argv
+  ;
+
+if (argv.help) {
+  console.log('Usage: ' + require('path').basename(process.argv[1]) + ' glob [glob...] [--rate=bytes/sec]');
+  process.exit();
+}
+if (typeof argv.pattern === 'string') {
+  argv.pattern = [argv.pattern];
+}
+require('../')({files: argv._, rate: argv.rate, patterns: argv.pattern, repeat: argv.repeat, ignore: argv.ignore}).pipe(process.stdout);
---
+++ index.js
@@ -0,0 +1,90 @@
+var glob = require('glob')
+  , fs = require('fs')
+  , Stream = require('stream')
+  , inherits = require('util').inherits
+  , brake = require('brake')
+  , format = require('util').format
+  ;
+
+module.exports = function prog(options) {
+  options || (options = {});
+  return new Prog(options).pipe(brake(options.rate || 100));
+};
+
+function Prog(options) {
+  Stream.call(this);
+  this.readable = true;
+  this.writable = true;
+  this.stack = [];
+  this.pile = [];
+  this.started = false;
+  if (typeof options.ignore === 'undefined') {
+    options.ignore = '/?node_modules/';
+  }
+  this.ignore = options.ignore && new RegExp(options.ignore);
+  this.repeat = options.repeat;
+  if (options.files && options.files.length) {
+    options.files.forEach(this.onMatch.bind(this));
+  }
+  else {
+    (options.patterns || ['*.json', '*.js', '*.coffee', '**/*.js', '**/*.coffee', '*.sh', '**/*.sh', '*.md', '*.markdown'])
+      .forEach(this.addPattern.bind(this));
+  }
+}
+inherits(Prog, Stream);
+module.exports.Prog = Prog;
+
+Prog.prototype.addPattern = function(pattern) {
+  var self = this;
+  glob(pattern)
+    .on('match', this.onMatch.bind(this))
+    .on('end', function() {
+      if (!self.started) self.emit('end');
+    });
+};
+
+Prog.prototype.onMatch = function(file) {
+  if (this.ignore && this.ignore.test(file)) return;
+  this.stack.push(file);
+  if (!this.started) {
+    this.started = true;
+    this.end();
+  }
+}
+
+Prog.prototype.write = function(chunk) {
+  this.emit('data', chunk);
+};
+
+Prog.prototype.end = function(chunk) {
+  if (chunk) {
+    this.write(chunk);
+  }
+  var file = this.stack.pop();
+  if (!file) {
+    this.emit('data', '\n');
+    if (this.repeat) {
+      this.stack = this.pile;
+      this.pile = [];
+      this.end();
+      return;
+    }
+    this.emit('end');
+    return;
+  }
+  this.pile.unshift(file);
+  this.header(file);
+  fs.createReadStream(file).pipe(this);
+};
+
+Prog.prototype.header = function(file) {
+  var sep = repeat('-', file.length);
+  if (this.started) this.emit('data', '\n');
+  this.emit('data', format('%s\n%s\n%s\n', sep, file, sep));
+};
+
+function repeat(str, len) {
+  var ret = '';
+  while (ret.length < len) ret += str;
+  return ret;
+}
